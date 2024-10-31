735695f1e04b6264e0710a3d452545957ee8f2ff
Anish Thapa <s2024006@ed.ac.uk>
2024-10-31 14:26:29 +0000

    Add server status and clients

--- grpc_start/client.py
+++ grpc_start/client.py
@@ -19,12 +19,26 @@ def run():
     print("Will try to greet world ...")
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = lock_pb2_grpc.LockServiceStub(channel)
-        # call all methods:
+
+        # Initialise client
         response = stub.client_init(lock_pb2.Int(rc=0))
         print("client_init received: " + str(response.rc))
+
+        # Acquire lock
         response = stub.lock_acquire(lock_pb2.lock_args(client_id=1))
         print("lock_acquire received: " + str(response.status))
 
+        # Release lock
+        response = stub.lock_release(lock_pb2.lock_args(client_id=1))
+        print("lock_release received: " + str(response.status))
+
+        # Append to file
+        response = stub.file_append(lock_pb2.file_args(filename="file_1", content="Hello".encode(), client_id=1))
+        print("file_append received: " + str(response.status))
+
+        # Close client
+        response = stub.client_close(lock_pb2.Int(rc=0))
+        print("client_close received: " + str(response.rc))
 
 if __name__ == "__main__":
     logging.basicConfig()
