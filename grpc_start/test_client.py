a2f4238c9f6270b6ac9651fa0aace0bc1388f412
Neel Amonkar <studentneel@gmail.com>
2024-10-31 13:52:28 +0000

    stuff

--- grpc_start/test_client.py
+++ grpc_start/test_client.py
@@ -2,35 +2,24 @@ import grpc
 import lock_pb2_grpc
 import lock_pb2
 
-channel = grpc.insecure_channel('localhost:50051')
-stub = lock_pb2_grpc.LockServiceStub(channel)
+def RPC_init():
+    raise NotImplementedError("pbbbbbt")
 
-response = stub.client_init(lock_pb2.Int(rc=0))
-print(str(response.status))
+def RPC_lock_acquire():
+    raise NotImplementedError("pbbbbbt")
 
-# from __future__ import print_function
+def RPC_lock_release():
+    raise NotImplementedError("pbbbbbt")
 
-# import logging
+def RPC_append_file():
+    raise NotImplementedError("pbbbbbt")
 
-# import grpc
-# import lock_pb2
-# import lock_pb2_grpc
+def RPC_close():
+    raise NotImplementedError("pbbbbbt")
 
+if __name__ == "__main__":
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = lock_pb2_grpc.LockServiceStub(channel)
 
-# def run():
-#     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-#     # used in circumstances in which the with statement does not fit the needs
-#     # of the code.
-#     print("Will try to greet world ...")
-#     with grpc.insecure_channel("localhost:50051") as channel:
-#         stub = lock_pb2_grpc.LockServiceStub(channel)
-#         # call all methods:
-#         response = stub.client_init(lock_pb2.Int(rc=0))
-#         print("client_init received: " + str(response.rc))
-#         response = stub.lock_acquire(lock_pb2.lock_args(client_id=1))
-#         print("lock_acquire received: " + str(response.status))
-
-
-# if __name__ == "__main__":
-#     logging.basicConfig()
-#     run()
+    response = stub.client_init(lock_pb2.Int(rc=0))
+    print(str(response.rc))
--- grpc_start/test_server.py
+++ grpc_start/test_server.py
@@ -1,25 +1,43 @@
 from concurrent import futures
+from collections import deque
 
 import grpc
 import lock_pb2
 import lock_pb2_grpc
 
 class LockServer(lock_pb2_grpc.LockServiceServicer):
+    def __init__(self):
+        self.locked = False
+        self.waiting_list = deque()
+
     def client_init(self, request, context):
         print("client_init called")
-        return lock_pb2.Response(status=lock_pb2.Status.SUCCESS) # is this sensible????
+        return lock_pb2.Int(rc=0)
     
     def lock_acquire(self, request, context):
+        if self.locked:
+
+        else:
+            self.locked = True
+            return lock_pb2.Response(status=)
+    
+    def lock_release(self, request, context):
+        raise NotImplementedError("pbbbbbt")
+    
+    def file_append(self, request, context):
         raise NotImplementedError("pbbbbbt")
     
     def client_close(self, request, context):
         raise NotImplementedError("pbbbbbt")
         
+def start_server():
+    port = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    lock_pb2_grpc.add_LockServiceServicer_to_server(LockServer(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()   
 
-port = "50051"
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-lock_pb2_grpc.add_LockServiceServicer_to_server(LockServer(), server)
-server.add_insecure_port("[::]:" + port)
-server.start()
-print("Server started, listening on " + port)
-server.wait_for_termination()   
+if __name__ == "__main__":
+    start_server()
