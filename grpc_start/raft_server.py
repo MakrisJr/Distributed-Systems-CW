594d29f2fb64f6a60f142acbf662b09f025d1209
Anish Thapa <s2024006@ed.ac.uk>
2024-11-12 19:45:05 +0000

    Implement append_entries

--- grpc_start/raft_server.py
+++ grpc_start/raft_server.py
@@ -21,13 +21,47 @@ class RaftServer(raft_pb2_grpc.RaftServiceServicer):
         self.next_index = 0
         self.match_index = 0
 
-        self.state = RaftServerState.CANDIDATE  # placeholder
+        self.state = RaftServerState.FOLLOWER  # placeholder
 
     # this bit is executed on the followers - this is the CONSEQUENCE of the RPC call, not the call itself
     def append_entries(self, request, context):
-        # return super().append_entries(request, context)
+        # Step 1
+        if request.term < self.current_term:
+            return raft_pb2.AppendResponse(term=self.current_term, success=False)
 
-        raise NotImplementedError
+        if request.term > self.current_term:
+            self.current_term = request.term
+            self.voted_for = None
+            self.state = RaftServerState.FOLLOWER
+
+        # Step 2
+        if (
+            len(self.log) <= request.prevLogIndex
+            and self.log[request.prevLogIndex].term != request.prevLogIndex
+        ):
+            return raft_pb2.AppendResponse(term=self.current_term, success=False)
+
+        # Step 3
+        index = request.prevLogIndex + 1
+        i = 0
+        for i, entry in enumerate(request.entries):
+            if index < len(self.log):
+                if self.log[index].term != entry.term:
+                    self.log = self.log[:index]
+                    break
+            else:
+                break
+            index += 1
+
+        # Step 4
+        for entry in request.entries[i:]:
+            self.log.append(entry)
+
+        # Step 5
+        if request.leaderCommit > self.commit_index:
+            self.commit_index = min(request.leaderCommit, len(self.log) - 1)
+
+        return raft_pb2.AppendResponse(term=self.current_term, success=True)
 
     # this bit is executed on the followers - this is the CONSEQUENCE of the RPC call, not the call itself
     def request_vote(self, request, context):
@@ -41,5 +75,5 @@ class RaftServer(raft_pb2_grpc.RaftServiceServicer):
 
     # this is where this server calls the append_entries rpc on other servers
     def send_append_entries(self):
-        if self.state == RaftServerState.LEADER:
-            raise NotImplementedError
+        if self.state != RaftServerState.LEADER:
+            return
